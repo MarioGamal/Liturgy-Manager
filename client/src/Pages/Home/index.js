6db0e484f2ce467ba7dfd5aae0bab12850a60a43
Mario Gamal Todary | Ejada Systems Ltd <mario.gamal15@gmail.com>
2020-07-29 10:58:23 +0200

    add church title

--- client/src/Pages/Home/index.js
+++ client/src/Pages/Home/index.js
@@ -11,6 +11,7 @@ const Home = ({ info, infoTitle, currentPhase }) => {
     const endDate = dayMonthFormat(currentPhase.end)
     return (
         <div>
+            <h2>كنيسة الشهيد العظيم مارجرجس - منشية الصدر</h2>
             <Title title={homeTitle} classes='mb-3 mt-3 text-center'
                 subTitle={`${startDate}
                      ـــــــــ ${endDate}`} />
